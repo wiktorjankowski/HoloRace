4b46d395fd1ac6321fd9ff493a42f7d97b8c953a
Wiktor Jankowski <wiktor.jankowski@welastic.pl>
2022-04-27 17:20:36 +0200

    Initial Commit

---
+++ HoloRace/HoloRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoloRaceApp.swift
+//  HoloRace
+//
+//  Created by Wiktor Jankowski on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HoloRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
